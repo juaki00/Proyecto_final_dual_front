45b56a6756776563867f0275036058e3cd398950
Joaquin <juaki00@gmail.com>
2023-05-09 19:26:38 +0200

    commit inicial

--- src/main/java/edu/fpdual/client/Main.java
+++ src/main/java/edu/fpdual/client/Main.java
@@ -1,6 +1,5 @@
-package edu.fpdual;
+package edu.fpdual.client;
 
-import edu.fpdual.client.dto.Usuario;
 import edu.fpdual.client.services.UserServices;
 
 public class Main {
@@ -8,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 //        System.out.println(new NotificationClient().ping());
-        System.out.println(new UserServices().insertUser(new Usuario("bbbbbb","bb","bb","bb","bb","bb")));
         System.out.println(new UserServices().getPass("w"));
         System.out.println(new UserServices().getAllUsers());
         System.out.println(new UserServices().userFromNick("bbbbbb"));
